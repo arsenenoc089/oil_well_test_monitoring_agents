8cc5ada10f75601f8232507720b77be9ff049a5a
The Trading Whisperer <29692338+thetradingwhisperer@users.noreply.github.com>
2025-05-15 09:49:10 +0000

    Added memory and interpreter agents

--- agents/main.py
+++ agents/main.py
@@ -1,30 +1,21 @@
 import asyncio
 from pydantic import BaseModel
-from agents import Agent, Runner, function_tool
+from agents import Agent, Runner, function_tool, trace
 from loguru import logger
 from utils import utils
+from tools.prompts import make_prompt, CONTEXT_PROMPT
 from dotenv import load_dotenv
 
+
+#Load environment variables
 load_dotenv()
+
 #Prompts
-def make_prompt(threshold=0.1):
-    ANOMALY_DETECTOR_PROMPT = (f'You are anomaly detection agent for well test data in an Oil and Gas company.'
-        '----------------------'
-        'You will be given some well test data (one at a time) for a specific well. '
-        'Look for anomalies in the oil rate, liquid rate or WCT (water cut). The data has some log of changes already computed '
-       f'log_diff_oil, log_diff_liq and log_diff_wct = ln(value/previous value). The thresold is + or - {threshold}. '
-        '----------------------'
-        'Anomalies are defined as below:'
-        '1 - Zonal test: you can detect a zonal test event (anomaly) if only two of the log_diff_z1bhp_meanbhp, log_diff_z2bhp_meanbhp'
-        f', log_diff_z3bhp_meanbhp are greater than {threshold} at any given time and the third one is below -{threshold}.'
-    )
+ANOMALY_DETECTOR_PROMPT, MEMORY_SAVER_PROMPT, INTERPRETATOR_PROMPT = make_prompt(threshold=0.1)
 
-    return ANOMALY_DETECTOR_PROMPT
-
-ANOMALY_DETECTOR_PROMPT = make_prompt(threshold=0.1)
-
-#Define the input schema
+#Agents output types
 class WellTestContext(BaseModel):
+    Date: str
     WellName: str
     WTLIQ: float
     WTOil: float
@@ -42,27 +33,96 @@ class WellTestContext(BaseModel):
     log_diff_thp: float
     log_diff_wct: float
 
+class ZonalTestMemory(BaseModel):
+    Date: str
+    WellName: str
+    Anomaly: bool
+    AnomalyType: str
+    WTLIQ: float
+    WTOil: float
+    WTTHP: float
+    WTWCT: float
+    Z1Status: str
+    Z2Status: str
+    Z3Status: str
+    Z1BHP: float
+    Z2BHP: float
+    Z3BHP: float
+
+class WellTestInterpretation(BaseModel):
+    Date: str
+    WellName: str
+    ZonalConfiguration: str
+    Interpretation: str
+    EngineerAction: str
+    InsightsSummary: str
+    """A short 2-3 sentence of your findings after analysing the data looking for anomalies"""
+
 class AnomalyInsights(BaseModel):
     short_summary: str
     """A short 2-3 sentence of your findings after analysing the data looking for anomalies"""
 
-    follow_up_questions: list[str]
-    """Suggested follow-up questions for analysis."""
 
-def get_test(df: dict) -> WellTestContext:
-    # Convert the input dictionary to a WellTestInput object
-    well_test_input = WellTestContext(**df)
-    return well_test_input
+# Tools
+@function_tool
+def save_test_memory(welldata: WellTestContext, file_path: str, 
+                    Anomaly: bool,
+                    AnomalyType: str,
+                    Z1Status: str,
+                    Z2Status: str,
+                    Z3Status: str) -> None:
+    """
+        Save the well test data record as a comma separated line in a text file.
+        Args:
+            Welldata: The well test data record to save.
+            file_path: The path to the file where the data will be saved.
+    """
+
+    file_path =  'agents/mem.txt'
+    logger.info(f"Saving well test data in memory")
+    fields = [
+        welldata.Date,
+        welldata.WellName,
+        Anomaly,
+        AnomalyType,
+        welldata.WTLIQ,
+        welldata.WTOil,
+        welldata.WTTHP,
+        welldata.WTWCT,
+        Z1Status,
+        Z2Status,
+        Z3Status,
+        welldata.Z1BHP,
+        welldata.Z2BHP,
+        welldata.Z3BHP        
+    ]
+    
+    with open(file_path, 'a') as f:
+        f.write(', '.join(map(str, fields)) + '\n')
 
 
+# Agent team
 anomaly_detection_agent = Agent(
     name="wt_anomaly_detector",
     instructions=ANOMALY_DETECTOR_PROMPT,
     output_type=AnomalyInsights,
+)
 
+MEMORY_SAVER_AGENT = Agent(
+    name="WT_memory_saver",
+    instructions=MEMORY_SAVER_PROMPT,
+    output_type=ZonalTestMemory,
+    tools=[save_test_memory],
+)
+
+INTERPRETATOR_AGENT = Agent(
+    name="wt_interpretator",
+    instructions=INTERPRETATOR_PROMPT,
+    output_type=WellTestInterpretation,
 )
 
 
+#Function to run the agents
 async def main():
     logger.info("Starting to load data")
     df = utils.load_transform_welltest_data('data/RMO_Agentic AI_train_test.xlsx')
@@ -70,21 +130,35 @@ async def main():
             'Z2BHP', 'Z3BHP', 'mean_bhp', 'log_diff_z1bhp_meanbhp',
             'log_diff_z2bhp_meanbhp', 'log_diff_z3bhp_meanbhp', 'log_diff_oil',
             'log_diff_liq', 'log_diff_thp', 'log_diff_wct']].copy()
+    df['Date'] = df['Date'].astype(str)
     
-    df = df.iloc[9:]
+    df = df.iloc[13:]
 
     df_iterator = df.iterrows()
     next(df_iterator)
     idx, serie = next(df_iterator)
     df = serie.to_dict()
-    context = get_test(df)
+
+    #Define the context
+    context = CONTEXT_PROMPT
 
     well_test_input = WellTestContext(**df)
     logger.info(f"well test input {well_test_input}")
-    result = await Runner.run(anomaly_detection_agent, input=f' Here are the well test data {well_test_input.model_dump()}' , context=context)
-    print(result.final_output)
 
+    # Ensure the entire workflow is a single trace
+    with trace("Deterministic story flow"):
 
+        # Run the anomaly detection agent
+        result_anomaly = await Runner.run(anomaly_detection_agent, input=f' Here are the well test data {well_test_input.model_dump()}' , context=context)
+        print(result_anomaly.final_output)
+
+        # Run the memory saver agent
+        result_memory = await Runner.run(MEMORY_SAVER_AGENT, input=f' Here are the well test data {well_test_input.model_dump()} and this is what the anomaly analysis result is {result_anomaly.final_output}' , context=context)
+        print(result_memory.final_output)
+
+        # Run the interpretator agent
+        result_interpretator = await Runner.run(INTERPRETATOR_AGENT, input=f' Here are the well test data {well_test_input.model_dump()} and this is what the anomaly analysis result is {result_anomaly.final_output}' , context=context)
+        print(result_memory.final_output)
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ agents/tools/prompts.py
@@ -0,0 +1,51 @@
+CONTEXT_PROMPT = (" The wells are horizontal well that have completion made of zones which can be opened or closed. Usually three zones. 1, 2 and 3."
+                    "Each zone has a BHP/pressure sensor which usually works unless it is broken. "
+                    "Wells are tested between every two weeks or every month, approximately."
+                    "--------------"
+                    "Normal well behavior is when the oil rate follows a decline curve aas expected and there is no sudden significant increase in water cut. "
+                    "An anomaly is anything that is not normal production behavior as expected. For example sigfnificant changes in rates (positive or negatives)"
+                    " or physical activities done by the engineers like zonal test, zonal optimisation, acid stimulation. Also increase water injection on nearby"
+                    "injectors can affect the observed water cut which is also an anomaly."
+                    "--------"
+                    "-> Sometimes an exercise known as zonal optimization is conducted that consists in shutting one or many zones (usually one) that has poor "
+                    "performance which can be relatively very high water cut (WCT) in comparison to the other zones. However, if the WCT is high and the zone "
+                    "still produces relatively high oil rate, the reservoir engineers can still decide to produce the zone. So a judgement call is need. "
+                    "For example zone1: WCT 20%, Zone 2,WCT 75%, Zone 3: WCT 35%. A decision can be made to shut/close zone 2 temporarily."
+                    "--------"
+                    "-> Another activity known as acid stimulation can be conducted to improve the flow of oil and water by default in the reservoir region near"
+                    " the wellbore. This acid usually dissolves deposits or reservoir material thereby increasing permeability. This usually leads to a sudden increase"
+                    " in oil rate that also decreases fast but overall a higher oil rate in the short term (2 months+). This is usally followed by an increase in BHP and"
+                    " a slight increase or decrease in WCT (water cut).")
+
+
+def make_prompt(threshold=0.1, file_path='agents/mem.txt'):
+    ANOMALY_DETECTOR_PROMPT = (f'You are anomaly detection agent for well test data in an Oil and Gas company.'
+        'You will be given some well test data (one at a time) for a specific well. '
+        'Look for anomalies in the oil rate, liquid rate or WCT (water cut). The data has some log of changes already computed '
+        f'log_diff_oil, log_diff_liq and log_diff_wct = ln(value/previous value). The thresold is + or - {threshold}. '
+        'Anomalies are defined as below:'
+        '1 - Zonal test: you can detect a zonal test event (anomaly) if only two of the log_diff_z1bhp_meanbhp, log_diff_z2bhp_meanbhp'
+        f', log_diff_z3bhp_meanbhp are greater than {threshold} at any given time and the third one is below -{threshold}.'
+        f'The zone with diff lower than the -{threshold} is the zone that is being tested (open) and the other two are closed.'
+        f'2 - Acid stimulation: you can detect an acid stimulation event (anomaly) if the there is a significant increase in oil production. log_diff_oil would be expected above {threshold}.'
+        '3 - Zonal optimization: you can detect a zonal optimization event (anomaly) if only one of the log_diff_z1bhp_meanbhp, log_diff_z2bhp_meanbhp, log_diff_z3bhp_meanbhp'
+        f' is greater than {threshold} and the other two are below -{threshold}.'
+    )
+
+    INTERPRETATOR_PROMPT = (
+        'You will be given a well test data and a summary of analysis from an anomaly detector agent.'
+        'you will have to interpret the data and give a generate your findings/interpretation. you need to inform the below:'
+        'Zonal Configuration: Commingle Production, Z1 Open, Z2 Open, Z3 Open, Z1 & Z2 Open etc...'
+        'Agent Interpretation: Transient, Zonal test, Acid stimulation, Zonal optimization, etc...'
+        'Engineer Action: No-Action - keep monitoring, Record zone performance from zone test, Analyse Zone performance, Zonal optimization recommended Acid stimulation recommended, etc...'
+        'Insights summary: A short 2-3 sentence of your findings after analysing the data land the highlighted anomalies'
+    )
+
+    MEMORY_SAVER_PROMPT = (
+        'You will be given a well test data and a summary of analysis from an anomaly detector agent.'
+        'Your job is to look into the anomaly analysis and look of zonal test is being mentioned.'
+        'If yes, then you need to save the data in a memory bank.'
+        f'filepath: {file_path}'
+    )
+
+    return ANOMALY_DETECTOR_PROMPT, MEMORY_SAVER_PROMPT ,INTERPRETATOR_PROMPT
